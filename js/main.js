5bafc0cc73f82d744ee5155066af1fbb72359fd7
midnight85 <maksvell2407@gmail.com>
2021-09-05 17:26:39 +0300

    register validation

--- js/main.js
+++ js/main.js
@@ -31,30 +31,30 @@ tabTriggers.addEventListener("click", (e) => {
 //         //===========
 //     });
 // });
-document.querySelector(".tab-trigger").click();
+// document.querySelector(".tab-trigger").click();
 // tabs //
 
 //  remember me
 
-const rmCheck = document.getElementById("rememberMe"),
-    emailInput = document.getElementById("email");
+// const rmCheck = document.getElementById("rememberMe"),
+//     emailInput = document.getElementById("email");
 
-if (localStorage.checkbox && localStorage.checkbox !== "") {
-    rmCheck.setAttribute("checked", "checked");
-    emailInput.value = localStorage.email;
-} else {
-    rmCheck.removeAttribute("checked");
-    emailInput.value = "";
-}
-rmCheck.addEventListener("click", () => {
-    if (rmCheck.checked && emailInput.value !== "") {
-        localStorage.email = emailInput.value;
-        localStorage.checkbox = rmCheck.value;
-    } else {
-        localStorage.email = "";
-        localStorage.checkbox = "";
-    }
-});
+// if (localStorage.checkbox && localStorage.checkbox !== "") {
+//     rmCheck.setAttribute("checked", "checked");
+//     emailInput.value = localStorage.email;
+// } else {
+//     rmCheck.removeAttribute("checked");
+//     emailInput.value = "";
+// }
+// rmCheck.addEventListener("click", () => {
+//     if (rmCheck.checked && emailInput.value !== "") {
+//         localStorage.email = emailInput.value;
+//         localStorage.checkbox = rmCheck.value;
+//     } else {
+//         localStorage.email = "";
+//         localStorage.checkbox = "";
+//     }
+// });
 
 //  remember me //
 
@@ -63,7 +63,7 @@ function showPass() {
     const activeTab = document.querySelector(".tab--active");
     const passwordDiv = activeTab.querySelector(".password-div");
     const passwordInput = activeTab.querySelector("input#password");
-    console.log(passwordInput.type);
+    // console.log(passwordInput.type);
     const passwordShowIcon = passwordDiv.querySelector(".password-toggle");
     passwordShowIcon.children[0].classList.toggle("password--show");
     if (passwordInput.type == "password") {
@@ -75,14 +75,52 @@ function showPass() {
 //  show password   //
 
 //  sign up check
+
+const form = document.querySelector(".form");
+const inputs = form.querySelectorAll(".form__input");
+// console.log(inputs);
 const terms = document.querySelector("#terms");
 const signupBtn = document.querySelector("#sign-up-btn");
-terms.addEventListener("change", (e) => {
-    if (terms.checked) {
-        signupBtn.classList.remove("disabled");
-    } else {
-        signupBtn.classList.add("disabled");
-    }
+// console.log(signupBtn);
+// signupBtn.preventDefault();
+const patterns = {
+    username: /^[a-z]/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+    phone: /^\d{7,14}$/,
+    password: /^(?!\s*$).+/,
+};
+termsCheck();
+form.addEventListener("submit", (e) => {
+    // console.log(e);
+    let err = false;
+    inputs.forEach((input) => {
+        let currentInput = input;
+        currentInput.value = currentInput.value.trim();
+        let result = patterns[currentInput.dataset.name];
+        if (!result.test(currentInput.value)) {
+            err = true;
+            input.classList.add("err");
+        }
+        if (err) {
+            e.preventDefault();
+        }
+        form.addEventListener("input", (e) => {
+            if (e.target.classList.contains("form__input")) {
+                e.target.classList.remove("err");
+            }
+        });
+    });
 });
+terms.addEventListener("change", termsCheck);
+
+function termsCheck() {
+    if (!terms.checked) {
+        signupBtn.classList.add("disabled");
+        signupBtn.setAttribute('disabled', 'true')
+    } else {
+        signupBtn.classList.remove("disabled");
+        signupBtn.removeAttribute('disabled')
+    }
+}
 
 //  sign up check   //
